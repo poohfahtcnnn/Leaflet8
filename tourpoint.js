3b552f207fd98137d9d1be9855e31f25337745b8
poohfahtcnnn <158020301+poohfahtcnnn@users.noreply.github.com>
2024-05-13 16:24:32 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+var tourpoint ={
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khao Yai National Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.49714287428634,
+            14.33166524494186
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Suankularb Wittayalai School",
+          "type": "school",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.4982559693949,
+            13.742883705544244
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "พระปฐมบรมราชานุสรณเฉลิมพระเกียรติ",
+          "type": "park",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.4982075164379,
+            13.741144973502088
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนสมเด็จพระปกเกล้าฯ",
+          "type": "park",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.49930183076549,
+            13.740681834390344
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ศูนย์กีฬาเฉลิมพระเกียรติ",
+          "type": "park",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.49266207055132,
+            13.646611401710231
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "King Mongkut's University of Technology",
+          "type": "university",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.49428662796055,
+            13.651095508821328
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Thonburirom Park",
+          "type": "park",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.4914614469667,
+            13.652290164521375
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khao Kho National Park",
+          "type": "park",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.94823919522617,
+            16.4927339413523
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Chai Son National Park",
+          "type": "park",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            99.44647559720335,
+            18.84220271808175
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Ban Pa Jiang Mae Ruam",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            99.3280544187736,
+            18.842472677425135
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
